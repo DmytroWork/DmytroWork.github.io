7dd6c608fd09416f8150e9942847193a832b1cbf
DmytroWork <135351054+DmytroWork@users.noreply.github.com>
2023-06-23 20:14:58 +0300

    final

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $("document").ready(function(){
-
 	$(".btn").hover(function(){
 		$("#header").toggleClass("active");
 	})
-	
+	$("#header .spec-btn").click(function(){
+		$("#header .btns-left").toggleClass("active")
+	})
 })
